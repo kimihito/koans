735429ef82b253f62099d80864b3fc561b37659a
kimihito <tatsurotamashiro@gmail.com>
2013-01-09 19:23:19 +0900

    177

--- triangle.rb
+++ triangle.rb
@@ -15,6 +15,20 @@
 #
 def triangle(a, b, c)
   # WRITE THIS CODE
+  
+  s = (a + b + c) / 2.0
+  ok = (s - a) * (s - b) * (s - c)
+  if a <= 0 || b <= 0 || c <= 0 || ok <= 0 then
+    raise TriangleError
+  end
+
+  if ((a == b) && (b == c) && (a == c))
+    return :equilateral
+  elsif ((a == b) || (b == c) || (a == c))
+    return :isosceles
+  else
+    return :scalene
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
